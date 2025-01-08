6750891b8c93d98c2f8b7ed998d431f0a534018e
James Grieve <jamesonrgrieve@gmail.com>
2025-01-07 19:33:43 -0700

    Super basic class with only fields;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,13 +2,19 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  // This is where the code you're actually experimenting with goes.
-
-  const prompt = "Please enter your name, or 'Exit' to quit: ";
-  let name = await input(prompt);
-
-  while (name !== "Exit") {
-    output("Hello, " + name + "!");
-    name = await input(prompt);
+  class Person {
+    firstName = "John";
+    lastName = "Doe";
+    birthDate = "January 1, 1970";
   }
+
+  const me = new Person();
+  me.firstName = "James";
+  me.lastName = "Grieve";
+  const you = new Person();
+
+  output(`${me.firstName} ${me.lastName}, born on ${me.birthDate}.`);
+  output(`${you.firstName} ${you.lastName}, born on ${you.birthDate}.`);
+
+
 }
