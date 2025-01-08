34d95939b33ad09e9e3866341d34f69441e2d9e0
James Grieve <jamesonrgrieve@gmail.com>
2025-01-07 19:40:24 -0700

    Constructor parameters with fallback assignments;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,18 +3,17 @@
 // eslint-disable-next-line no-unused-vars
 async function main() {
   class Person {
-    constructor() {
-      this.firstName = "John";
-      this.lastName = "Doe";
-      this.birthDate = "January 1, 1970";
-      output("Created a new Person object! Hurray!")
+    constructor(firstName, lastName, birthDate) {
+      this.firstName = firstName || "John";
+      this.lastName = lastName || "Doe";
+      this.birthDate = birthDate || "January 1, 1970";
     }
 
   }
 
-  const me = new Person();
-  me.firstName = "James";
-  me.lastName = "Grieve";
+  const me = new Person("James", "Grieve");
+  // me.firstName = "James";
+  // me.lastName = "Grieve";
   const you = new Person();
 
   output(`${me.firstName} ${me.lastName}, born on ${me.birthDate}.`);
