2edcb0e2d7bd46317f54c64c5c7b0f8e75f55c5e
James Grieve <jamesonrgrieve@gmail.com>
2025-01-10 19:11:32 -0700

    Solution pending test;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,45 +2,52 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  class Person {
-    #birthDate; // Private fields prefixed with # CANNOT be changed from outside the object/class. This means we know 100% all values in it (so long as you only set it in the setter) MUST be validated according to said setter.
-    constructor(firstName = "John", lastName = "Doe", birthDate = "January 1, 1970") {
-      this.firstName = firstName || "John";
-      this.lastName = lastName || "Doe";
-      this.birthDate = birthDate || "January 1, 1970";
-      this.hair = {
-        color: "black",
-        length: 5
+  class Pen {
+    #brand; // Private fields prefixed with # CANNOT be changed from outside the object/class. This means we know 100% all values in it (so long as you only set it in the setter) MUST be validated according to said setter.
+    #colour;
+    #inkLevel;
+    constructor(brand = "Bic", colour = "black", inkLevel = 100) {
+      this.brand = brand;
+      this.colour = colour;
+      this.inkLevel = inkLevel;
+    }
+    get brand() {
+      return this.#brand;
+    }
+    set brand(value) {
+      this.#brand = value;
+    }
+    get colour() {
+      return this.#colour;
+    }
+    set colour(value) {
+      this.#colour = value;
+    }
+    get inkLevel() {
+      return this.#inkLevel;
+    }
+    set inkLevel(value) {
+      if (value < 0 || value > 100) {
+        throw new Error("Ink level must be between 0 and 100 - it is a percentage.");
       }
-    }
-    get birthDate() {
-      return this.#birthDate.toUpperCase();
-    }
-    set birthDate(value) {
-      if ((new Date(value)).toString() !== "Invalid Date") {
-        this.#birthDate = value;
-      };
+      else {
+        this.#inkLevel = value;
+      }
     }
 
-    calculateAge() {
-      const today = new Date();
-      const birthDate = new Date(this.birthDate);
-      let age = today.getFullYear() - birthDate.getFullYear();
-      const m = today.getMonth() - birthDate.getMonth();
-      if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
+    write(numLetters) {
+      try {
+        this.inkLevel -= numLetters * 0.5;
       }
-      return age;
+      catch (error) {
+        output(error);
+      }
     }
   }
 
-  const me = new Person("James", "Grieve", "February 1, 1970");
-  // me.firstName = "James";
-  // me.lastName = "Grieve";
-  me.birthDate = "Yes";
-  const you = new Person();
-
-  output(`${me.firstName} ${me.lastName}, born on ${me.birthDate} - ${me.calculateAge()} years old with ${me.hair.color} hair.`);
-  output(`${you.firstName} ${you.lastName}, born on ${you.birthDate} - ${you.calculateAge()} years old.`);
-
+  const myPen = new Pen();
+  myPen.write(100);
+  myPen.write(42);
+  myPen.write(200);
+  output(myPen.inkLevel);
 }
