92c29f39307a19001eeda56ee24d83a1bb3e0d71
James Grieve <jamesonrgrieve@gmail.com>
2025-01-10 20:03:14 -0700

    Add unit tests;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,63 +1,65 @@
 // eslint-disable-next-line no-unused-vars
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
+class InkTube {
+  #inkAmountML = 150;
+  constructor(inkAmountML) {
+    this.inkAmountML = inkAmountML || this.inkAmountML;
+  }
+  get inkAmountML() {
+    return this.#inkAmountML.toFixed(2);
+  }
+  set inkAmountML(value) {
+    if (value < 0) {
+      throw new Error("Ink level must be zero or more.");
+    }
+    else {
+      this.#inkAmountML = Number(value);
+    }
+  }
+}
+class Pen {
+  #brand = "Bic"; // Private fields prefixed with # CANNOT be changed from outside the object/class. This means we know 100% all values in it (so long as you only set it in the setter) MUST be validated according to said setter.
+  #colour = "black";
+  inkTube;
+  constructor(brand, colour, inkAmountML) {
+    this.brand = brand || this.brand;
+    this.colour = colour || this.colour;
+    this.inkTube = new InkTube(inkAmountML);
+  }
+  get brand() {
+    return this.#brand;
+  }
+  set brand(value) {
+    this.#brand = value;
+  }
+  get colour() {
+    return this.#colour;
+  }
+  set colour(value) {
+    this.#colour = value;
+  }
+  write(numLetters) {
+    try {
+      this.inkTube.inkAmountML -= numLetters * 0.1;
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  toString() {
+    return JSON.stringify({
+      ...this,
+      brand: this.brand,
+      colour: this.colour
+    }, null, 4); // This number is the indentation
+  }
+}
+
 async function main() {
-  class InkTube {
-    constructor(inkAmountML) {
-      this.inkLevel = inkAmountML || this.inkAmountML;
-    }
-    #inkAmountML = 150;
-    get inkAmountML() {
-      return this.#inkAmountML;
-    }
-    set inkAmountML(value) {
-      if (value < 0) {
-        throw new Error("Ink level must be zero or more.");
-      }
-      else {
-        this.#inkAmountML = value.toFixed(2);
-      }
-    }
-  }
-  class Pen {
-    #brand = "Bic"; // Private fields prefixed with # CANNOT be changed from outside the object/class. This means we know 100% all values in it (so long as you only set it in the setter) MUST be validated according to said setter.
-    #colour = "black";
-    inkTube;
-    constructor(brand, colour, inkAmountML) {
-      this.brand = brand || this.brand;
-      this.colour = colour || this.colour;
-      this.inkTube = new InkTube(inkAmountML);
-    }
-    get brand() {
-      return this.#brand;
-    }
-    set brand(value) {
-      this.#brand = value;
-    }
-    get colour() {
-      return this.#colour;
-    }
-    set colour(value) {
-      this.#colour = value;
-    }
 
-    write(numLetters) {
-      try {
-        this.inkTube.inkAmountML -= numLetters * 0.1;
-      }
-      catch (error) {
-        output(error);
-      }
-    }
 
-    toString() {
-      return JSON.stringify({
-        ...this,
-        brand: this.brand,
-        colour: this.colour
-      }, null, 4); // This number is the indentation
-    }
-  }
 
   const myPen = new Pen();
   myPen.write(100);
@@ -68,3 +70,4 @@ async function main() {
 
 
 }
+module.exports = { Pen, InkTube };
