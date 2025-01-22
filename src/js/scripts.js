b6cbdbb01016500dabe290bf52df65d65f4dc66e
James Grieve <jamesonrgrieve@gmail.com>
2025-01-21 19:45:24 -0700

    Add a tile wrapper and caption; Add CSS;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,9 +6,10 @@ const gallery = document.querySelector('#gallery');
 newImageForm.addEventListener('submit', (event) => {
     event.preventDefault();
     // Create a div to wrap the image.
+    const newImageTile = document.createElement('div');
     const newImageWrapper = document.createElement('div');
     // Add the 'image' class to the div.
-    newImageWrapper.classList.add('image');
+    newImageTile.classList.add('image');
     // Create the image element.
     const newImage = document.createElement('img');
     // Set the image source and alt text.
@@ -17,8 +18,15 @@ newImageForm.addEventListener('submit', (event) => {
     newImage.title = newImageTitleInput.value;
     // Add the image to the wrapper.
     newImageWrapper.appendChild(newImage);
+    // Create the caption element.
+    const newImageCaption = document.createElement('p');
+    newImageCaption.textContent = newImageTitleInput.value;
+
+
     // Add the wrapper to the gallery.
-    gallery.appendChild(newImageWrapper);
+    newImageTile.appendChild(newImageWrapper);
+    newImageTile.appendChild(newImageCaption);
+    gallery.appendChild(newImageTile);
     // Clear the input fields.
     newImageTitleInput.value = '';
     newImageURLInput.value = '';
