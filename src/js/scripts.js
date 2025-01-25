8af1d84a4e3158d25c4ce1bbf54bfa00006aa97c
James Grieve <jamesonrgrieve@gmail.com>
2025-01-24 19:43:47 -0700

    Bonus;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,16 @@
-const myButtons = document.querySelectorAll('button');
+const myButtons = document.querySelectorAll('button:not(#create)');
+const createButton = document.querySelector('#create');
 
 for (const myButton of myButtons) {
     myButton.addEventListener('click', () => {
         myButton.remove();
     });
 }
+createButton.addEventListener('click', () => {
+    const newButton = document.createElement('button');
+    newButton.textContent = 'Remove Me!';
+    newButton.addEventListener('click', () => {
+        newButton.remove();
+    });
+    document.body.appendChild(newButton);
+});
