b46a812247a1e6acd08412dd9ed168241cb7dc84
James Grieve <jamesonrgrieve@gmail.com>
2025-01-28 20:14:57 -0700

    Timeouts and intervals;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,8 +3,25 @@ const myInput = document.querySelector('input');
 const myButton = document.querySelector('button');
 
 myButton.addEventListener('click', () => {
-    const newListItem = document.createElement('li');
-    newListItem.textContent = myInput.value;
-    myList.appendChild(newListItem);
-    myInput.value = '';
+  const newListItem = document.createElement('li');
+  newListItem.textContent = myInput.value;
+  myList.appendChild(newListItem);
+  myInput.value = '';
 });
+
+
+setTimeout(() => {
+  console.log('Hello, world!');
+}, 5000);
+setTimeout(() => {
+  console.log('Hello, world!');
+}, 6000);
+
+let counter = 0;
+const interval = setInterval(() => {
+  counter++;
+  console.log('Counter:', counter);
+  if (counter === 10) {
+    clearInterval(interval);
+  }
+}, 1000);
