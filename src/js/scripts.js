3ebb2ce5082777b03f0baefd880957b042d3c16d
James Grieve <jamesonrgrieve@gmail.com>
2025-02-04 20:22:55 -0700

    Fetch example in DOM;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,13 +1,15 @@
-// eslint-disable-next-line no-unused-vars
-/* global output, input */
-// eslint-disable-next-line no-unused-vars
-async function main() {
+const target = document.querySelector("#output");
+
+async function populateUser() {
   const response = await fetch("https://randomuser.me/api/");
   const data = await response.json();
   const person = data.results[0];
-  output("Name: " + person.name.first + " " + person.name.last);
-  output("Email: " + person.email);
-  output("Phone: " + person.phone);
-  output("Location: " + person.location.city + ", " + person.location.state + ", " + person.location.country);
+  const strings = ["Name: " + person.name.first + " " + person.name.last, "Email: " + person.email, "Phone: " + person.phone, "Location: " + person.location.city + ", " + person.location.state + ", " + person.location.country];
+  for (const string of strings) {
+    const listItem = document.createElement("li");
+    listItem.innerText = string;
+    target.appendChild(listItem);
+  }
 }
 
+populateUser();
