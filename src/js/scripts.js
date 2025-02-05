c8b9fa2de31bee59856cacdd8643be310b2ed238
James Grieve <jamesonrgrieve@gmail.com>
2025-02-04 19:18:40 -0700

    How async works;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,45 +2,23 @@
 /* global output, input */
 // eslint-disable-next-line no-unused-vars
 async function main() {
-  class Person {
-    #birthDate; // Private fields prefixed with # CANNOT be changed from outside the object/class. This means we know 100% all values in it (so long as you only set it in the setter) MUST be validated according to said setter.
-    constructor(firstName = "John", lastName = "Doe", birthDate = "January 1, 1970") {
-      this.firstName = firstName || "John";
-      this.lastName = lastName || "Doe";
-      this.birthDate = birthDate || "January 1, 1970";
-      this.hair = {
-        color: "black",
-        length: 5
-      }
-    }
-    get birthDate() {
-      return this.#birthDate.toUpperCase();
-    }
-    set birthDate(value) {
-      if ((new Date(value)).toString() !== "Invalid Date") {
-        this.#birthDate = value;
-      };
-    }
+  const request1 = wait(3000, () => {
+    output("First request done.");
+  });
+  const request2 = wait(5000, () => {
+    output("Second request done.");
+  });
+  const request3 = wait(2000, () => {
+    output("Third request done.");
+  });
 
-    calculateAge() {
-      const today = new Date();
-      const birthDate = new Date(this.birthDate);
-      let age = today.getFullYear() - birthDate.getFullYear();
-      const m = today.getMonth() - birthDate.getMonth();
-      if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-      }
-      return age;
-    }
-  }
 
-  const me = new Person("James", "Grieve", "February 1, 1970");
-  // me.firstName = "James";
-  // me.lastName = "Grieve";
-  me.birthDate = "Yes";
-  const you = new Person();
+  await Promise.all([request1, request2, request3]);
 
-  output(`${me.firstName} ${me.lastName}, born on ${me.birthDate} - ${me.calculateAge()} years old with ${me.hair.color} hair.`);
-  output(`${you.firstName} ${you.lastName}, born on ${you.birthDate} - ${you.calculateAge()} years old.`);
+}
 
+async function wait(ms, callback) {
+  const timeout = new Promise((res) => setTimeout(res, ms));
+  await timeout;
+  callback();
 }
