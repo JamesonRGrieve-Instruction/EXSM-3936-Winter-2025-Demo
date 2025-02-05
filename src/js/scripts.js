16772bd09b5fe0ebe628c8d8802950ba93e8603b
James Grieve <jamesonrgrieve@gmail.com>
2025-02-04 20:32:49 -0700

    Use 'then' to avoid 'await';

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,14 +1,13 @@
 const target = document.querySelector("#output");
 
-async function populateUser() {
-  const data = await (await fetch("https://randomuser.me/api/")).json();
-  const person = data.results[0];
+fetch("https://randomuser.me/api/").then((result) => {
+  return result.json();
+}).then((result) => {
+  const person = result.results[0];
   const strings = ["Name: " + person.name.first + " " + person.name.last, "Email: " + person.email, "Phone: " + person.phone, "Location: " + person.location.city + ", " + person.location.state + ", " + person.location.country];
   for (const string of strings) {
     const listItem = document.createElement("li");
     listItem.innerText = string;
     target.appendChild(listItem);
   }
-}
-
-populateUser();
+});
