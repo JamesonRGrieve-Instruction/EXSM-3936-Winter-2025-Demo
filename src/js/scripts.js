3b9ca703612475f6d042348f305d612d6b62be88
James Grieve <jamesonrgrieve@gmail.com>
2025-02-04 20:29:30 -0700

    Chaining awaits;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,7 @@
 const target = document.querySelector("#output");
 
 async function populateUser() {
-  const response = await fetch("https://randomuser.me/api/");
-  const data = await response.json();
+  const data = await (await fetch("https://randomuser.me/api/")).json();
   const person = data.results[0];
   const strings = ["Name: " + person.name.first + " " + person.name.last, "Email: " + person.email, "Phone: " + person.phone, "Location: " + person.location.city + ", " + person.location.state + ", " + person.location.country];
   for (const string of strings) {
