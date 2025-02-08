cd2da246bc0141dd4fb51643595d84634ce101f0
James Grieve <jamesonrgrieve@gmail.com>
2025-02-07 19:21:49 -0700

    Bonus;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,8 @@
 const sendButton = document.getElementById('sendButton');
+const resetButton = document.getElementById('resetButton');
 const messageLog = document.querySelector('.messageLog');
 const input = document.querySelector('input');
+const timers = [];
 sendButton.addEventListener('click', () => {
   const message = input.value;
   const newP = document.createElement('p');
@@ -8,9 +10,16 @@ sendButton.addEventListener('click', () => {
   messageLog.appendChild(newP);
   input.value = '';
   input.focus();
-  setTimeout(() => {
+  timers.push(setTimeout(() => {
     const newP = document.createElement('p');
     newP.textContent = `[${(new Date().toLocaleTimeString())}] AI: ipsum dolor sit amet.`;
     messageLog.appendChild(newP);
-  }, Math.ceil(Math.random() * 30) * 1000);
+  }, Math.ceil(Math.random() * 30) * 1000));
+});
+resetButton.addEventListener('click', () => {
+  messageLog.innerHTML = '';
+  for (const timer of timers) {
+    clearTimeout(timer);
+  }
+  timers.length = 0;
 });
