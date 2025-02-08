14c2e5d65cccc0b3452476a6503c19e60a8810b2
James Grieve <jamesonrgrieve@gmail.com>
2025-02-07 20:02:50 -0700

    Add ID's and refs;

---
+++ src/js/scripts.js
@@ -0,0 +1,4 @@
+const pendingTodos = document.querySelector('#pendingTodos');
+const completedTodos = document.querySelector('#completedTodos');
+const addTodo = document.querySelector('#addTodo');
+const clearCompletedTodos = document.querySelector('#clearCompletedTodos');
