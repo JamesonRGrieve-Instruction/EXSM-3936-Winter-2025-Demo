103386c5f793abccafd2f579d28b9279a8fe7feb
James Grieve <jamesonrgrieve@gmail.com>
2025-02-07 19:07:57 -0700

    Message system skeleton;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,16 +1,1 @@
-const myButtons = document.querySelectorAll('button:not(#create)');
-const createButton = document.querySelector('#create');
 
-for (const myButton of myButtons) {
-    myButton.addEventListener('click', () => {
-        myButton.remove();
-    });
-}
-createButton.addEventListener('click', () => {
-    const newButton = document.createElement('button');
-    newButton.textContent = 'Remove Me!';
-    newButton.addEventListener('click', () => {
-        newButton.remove();
-    });
-    document.body.appendChild(newButton);
-});
