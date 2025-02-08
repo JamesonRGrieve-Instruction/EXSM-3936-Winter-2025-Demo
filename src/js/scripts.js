1ba48f268ee28fb0cbd68e25747b45b48620af40
James Grieve <jamesonrgrieve@gmail.com>
2025-02-07 20:10:25 -0700

    Base spec;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,3 +2,32 @@ const pendingTodos = document.querySelector('#pendingTodos');
 const completedTodos = document.querySelector('#completedTodos');
 const addTodo = document.querySelector('#addTodo');
 const clearCompletedTodos = document.querySelector('#clearCompletedTodos');
+const input = document.querySelector('input');
+addTodo.addEventListener('click', () => {
+  if (input.value.trim() !== '') {
+    const newTodo = document.createElement('li');
+    const newTodoP = document.createElement('p');
+    newTodoP.textContent = input.value;
+
+    input.value = '';
+
+    const newCheck = document.createElement('input');
+    newCheck.type = 'checkbox';
+    newTodo.appendChild(newCheck);
+    newTodo.appendChild(newTodoP);
+    newCheck.addEventListener('change', () => {
+      if (newCheck.checked) {
+        pendingTodos.removeChild(newTodo);
+        completedTodos.appendChild(newTodo);
+      } else {
+        completedTodos.removeChild(newTodo);
+        pendingTodos.appendChild(newTodo);
+      }
+    });
+    pendingTodos.appendChild(newTodo);
+
+  }
+});
+clearCompletedTodos.addEventListener('click', () => {
+  completedTodos.innerHTML = '';
+});
