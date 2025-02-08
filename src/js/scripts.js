6777f60b359fcbc481b469a3f9487749c4ba638e
James Grieve <jamesonrgrieve@gmail.com>
2025-02-07 19:14:19 -0700

    Base spec;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,16 @@
-
+const sendButton = document.getElementById('sendButton');
+const messageLog = document.querySelector('.messageLog');
+const input = document.querySelector('input');
+sendButton.addEventListener('click', () => {
+  const message = input.value;
+  const newP = document.createElement('p');
+  newP.textContent = `[${(new Date().toLocaleTimeString())}] You: ${message}`;
+  messageLog.appendChild(newP);
+  input.value = '';
+  input.focus();
+  setTimeout(() => {
+    const newP = document.createElement('p');
+    newP.textContent = `[${(new Date().toLocaleTimeString())}] AI: ipsum dolor sit amet.`;
+    messageLog.appendChild(newP);
+  }, Math.ceil(Math.random() * 30) * 1000);
+});
