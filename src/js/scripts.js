4c3de6a83edd3a96c9a8591e490f5b6372c9c089
James Grieve <jamesonrgrieve@gmail.com>
2025-02-14 19:27:09 -0700

    Solution;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,13 +1,37 @@
-const target = document.querySelector("#output");
+const search = document.querySelector("#input");
+const go = document.querySelector("#go");
+const output = document.querySelector("#output");
 
-fetch("https://randomuser.me/api/").then((result) => {
-  return result.json();
-}).then((result) => {
-  const person = result.results[0];
-  const strings = ["Name: " + person.name.first + " " + person.name.last, "Email: " + person.email, "Phone: " + person.phone, "Location: " + person.location.city + ", " + person.location.state + ", " + person.location.country];
-  for (const string of strings) {
-    const listItem = document.createElement("li");
-    listItem.innerText = string;
-    target.appendChild(listItem);
-  }
+go.addEventListener("click", () => {
+  output.innerHTML = "";
+  const pokemon = search.value;
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((pokemon) => {
+      const name = document.createElement("h2");
+      name.textContent = pokemon.name;
+      const sprite = document.createElement("img");
+      sprite.src = pokemon.sprites.front_default;
+      const types = document.createElement("p");
+      types.textContent = "Types: ";
+      for (let i = 0; i < pokemon.types.length; i++) {
+        if (i > 0) {
+          types.textContent += ", ";
+        }
+        types.textContent += pokemon.types[i].type.name;
+      }
+      const weight = document.createElement("p");
+      weight.textContent = `Weight: ${pokemon.weight}`;
+      const height = document.createElement("p");
+      height.textContent = `Height: ${pokemon.height}`;
+      const experience = document.createElement("p");
+      experience.textContent = `Base Experience: ${pokemon.base_experience}`;
+      output.appendChild(name);
+      output.appendChild(sprite);
+      output.appendChild(types);
+      output.appendChild(weight);
+      output.appendChild(height);
+      output.appendChild(experience);
+    });
 });
