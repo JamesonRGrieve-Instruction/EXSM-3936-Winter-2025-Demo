fa34d2462963e91bad0f05e763e222f5c3e5873e
James Grieve <jamesonrgrieve@gmail.com>
2025-02-14 19:13:15 -0700

    Add clear button that we forgot;

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,7 @@
 const theNumber = document.getElementById("number");
 const input = document.getElementById("input");
 const submit = document.getElementById("submit");
-
+const clear = document.getElementById("clear");
 if (localStorage.getItem("theNumber")) {
   theNumber.textContent = localStorage.getItem("theNumber");
 } else {
@@ -11,4 +11,8 @@ if (localStorage.getItem("theNumber")) {
 submit.addEventListener("click", () => {
   theNumber.textContent = Number(theNumber.textContent) + Number(input.value);
   localStorage.setItem("theNumber", theNumber.textContent);
+});
+clear.addEventListener("click", () => {
+  theNumber.textContent = "0";
+  localStorage.setItem("theNumber", "0");
 });
